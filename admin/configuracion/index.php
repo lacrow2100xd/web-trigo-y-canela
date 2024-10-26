889a4b854009d6073ece16add7a0cf2741ab6299
=lacrow2100xd <lacrow2100xd@gmail.com>
2024-10-26 09:15:32 -0500

    actualizacion 3

---
+++ admin/configuracion/index.php
@@ -0,0 +1,136 @@
+<?php
+
+
+require_once '../config/database.php';
+require_once '../config/config.php';
+require_once '../clases/cifrado.php';
+
+if(!isset($_SESSION['user_type'])){
+    header('Location: ../index.php');
+    exit;
+}
+
+if($_SESSION['user_type'] != 'admin'){
+    header('Location: ../../index.php');
+    exit;
+}
+
+require_once '../header.php';
+
+$db = new DataBase();
+$con = $db->conectar();
+
+$sql = "SELECT nombre, valor FROM configuracion";
+$resultado = $con->query($sql);
+$datos = $resultado->fetchAll(PDO::FETCH_ASSOC);
+
+$config = [];
+
+foreach($datos as $dato){
+    $config[$dato['nombre']] = $dato['valor'];
+}
+
+
+
+?>
+
+<main>
+    <div class="container-fluid px-4">
+        <h2 class="mt-4">Configuracion</h2>
+
+        <form action="guarda.php" method="post">
+        <ul class="nav nav-tabs mt-4" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">General</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Correo electrónico</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Paypal</button>
+            </li>
+        </ul>
+        <div class="tab-content mt-4" id="myTabContent">
+            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                    <div class="row mb-3">
+                        <div class="col-6">
+                            <label for="nombre">Nombre</label>  
+                            <input class="form-control" type="text" name="nombre" id="nombre" value="<?php echo $config['tienda_nombre']; ?>">
+                        </div>
+                        <div class="col-6">
+                            <label for="telefono">Telefono</label>  
+                            <input class="form-control" type="text" name="telefono" id="telefono" value="<?php echo $config['tienda_telefono']; ?>">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-6">
+                            <label for="moneda">Moneda</label>  
+                            <input class="form-control" type="text" name="moneda" id="moneda"value="<?php echo $config['tienda_moneda']; ?>">
+                        </div>
+                       
+                    </div>
+
+                    <div class="row mt-4">
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary">Guardar</button>
+                        </div>
+                    </div>
+            </div>
+            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                    <div class="row mb-3">
+                        <div class="col-6">
+                            <label for="smtp">SMTP</label>  
+                            <input class="form-control" type="text" name="smtp" id="smtp" value="<?php echo $config['correo_smtp']; ?>">
+                        </div>
+                        <div class="col-6">
+                            <label for="puerto">Puerto</label>  
+                            <input class="form-control" type="text" name="puerto" id="puerto" value="<?php echo $config['correo_puerto']; ?>">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <label for="email">Correo electrónico</label>  
+                            <input class="form-control" type="text" name="email" id="email"value="<?php echo $config['correo_email']; ?>">
+                        </div>
+                        <div class="col-6">
+                            <label for="password">Contraseña</label>  
+                            <input class="form-control" type="password" name="password" id="password"value="<?php echo $config['correo_password']; ?>">
+                        </div>     
+                    </div>
+
+                    <div class="row mt-4">
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary">Guardar</button>
+                        </div>
+                    </div>
+
+            </div>
+            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+                    <div class="row mb-3">
+                        <div class="col-9">
+                            <label for="paypal_cliente">Cliente ID</label>  
+                            <input class="form-control" type="text" name="paypal_cliente" id="paypal_cliente" value="<?php echo $config['paypal_cliente']; ?>">
+                        </div>
+                        <div class="col-3">
+                            <label for="paypal_moneda">Moneda</label>  
+                            <input class="form-control" type="text" name="paypal_moneda" id="paypal_moneda" value="<?php echo $config['paypal_moneda']; ?>">
+                        </div>
+                    </div>
+
+                    <div class="row mt-4">
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary">Guardar</button>
+                        </div>
+                    </div>
+
+            </div>
+        </div>
+
+    </div>
+</main>
+
+
+<?php 
+
+require_once '../layaouts/Footer.php';
+?>
